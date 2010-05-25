d7975f8aa0f01181bd00c9ae22030695f4a04c27
Gary Johnson <gwjohnso@uvm.edu>
2010-05-25 19:11:43 -0400

    Adding swank-clojure to project.clj dependencies list.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
                 al.) framework for Ecosystem Service Assessment."
   :dependencies [[org.clojure/clojure "1.0.0"]
 		 [org.clojure/clojure-contrib "1.0.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
+  :dev-dependencies [[org.clojars.jwr/lein-swank "1.1.0"]
 		     [lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces   [clj-span.commandline])
