a9a6b9313bde722a0aa872820d32246b66e8f50a
Gary Johnson <gwjohnso@uvm.edu>
2010-12-04 15:24:13 -0500

    1) Made three project-1.{0,1,2}.clj scripts to try and use different    versions of clojure with clj-span.
    2) Renamed spit to duck-spit in clj-span.worldgen so that clj-span
       will compile cleanly against both clojure-1.2 and clojure-1.0.

--- project-1.1.clj
+++ project-1.1.clj
@@ -2,8 +2,8 @@
   :description "clj-span is an implementation of the Service Path
                 Attribution Networks (Springer LNCS 2010 - Johnson et
                 al.) framework for Ecosystem Service Assessment."
-  :dependencies     [[org.clojure/clojure "1.2.0"]
-                     [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies     [[org.clojure/clojure "1.1.0"]
+                     [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0"]]
   :main             clj-span.commandline)
