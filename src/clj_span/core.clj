a198b776486590f00f925503d86764ea93c7ec3f
Gary Johnson <gwjohnso@uvm.edu>
2010-12-04 14:03:14 -0500

    1) Added "SurfaceWaterMovement" as a valid flow-model in    clj-span.core, imported clj-span.water-model into clj-span.core,    and setup clj-span.aries-span-bridge to attach the Hydrosheds layer    to the flow-layers map for this new model.
    2) Comments added to clj-span.sediment-model describing how to handle
       multiple use types in one model run.

--- src/clj_span/core.clj
+++ src/clj_span/core.clj
@@ -54,6 +54,7 @@
                                         blocked-flow
                                         actual-flow)])
   (:require clj-span.flood-model
+            clj-span.water-model
             clj-span.carbon-model
             clj-span.sediment-model
             clj-span.proximity-model
@@ -148,7 +149,7 @@
           (number>=1?  downscaling-factor)
           (every? #{:finite :infinite} [source-type sink-type use-type])
           (#{:rival :non-rival} benefit-type)
-          (#{"LineOfSight" "Proximity" "CO2Removed" "FloodWaterMovement" "Sediment"} flow-model)
+          (#{"LineOfSight" "Proximity" "CO2Removed" "FloodWaterMovement" "SurfaceWaterMovement" "Sediment"} flow-model)
           (#{:cli-menu :closure-map} result-type)]})
   ;; Initialize global parameters
   (set-global-params! {:rv-max-states      rv-max-states
