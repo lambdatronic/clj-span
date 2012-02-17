b5e6cc7d05fa55310ead10b37f4db011ab041cf2
Gary Johnson <gwjohnso@uvm.edu>
2012-02-17 12:00:53 -0500

    Updating README and project.clj.

--- project.clj
+++ project.clj
@@ -1,1 +1,18 @@
-project-1.2.clj
+(defproject clj-span "1.0.0-SNAPSHOT"
+  :description
+"clj-span - SPAN models for Ecosystem Service Assessment
+Copyright 2009-2012 Gary W. Johnson (gjohnson@green-compass.org)
+----------------------------------------------------------------------
+This application provides a suite of spatial models that simulate the
+flow of ecosystem services from the landscapes which provide them to
+the people who receive them in a region determined by the user's input
+maps. This implementation is based on the paper 'Service Path
+Attribution Networks (SPANs): Spatially Quantifying the Flow of
+Ecosystem Services from Landscapes to People' (Springer LNCS 2010 -
+Johnson et al.)
+----------------------------------------------------------------------"
+  :dependencies [[org.clojure/clojure         "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :checksum-deps true
+  ;; :warn-on-reflection true
+  :main clj-span.commandline)
