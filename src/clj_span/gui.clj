7827a40b2efdd5ad90fe25e09da54eb7c57e900a
Gary Johnson <gwjohnso@uvm.edu>
2012-03-12 18:14:33 -0400

    Preventing clj-span.gui/{draw-layer,draw-ref-layer,write-layer-to-file} from outputting stdev images if value-type = :numbers.

--- src/clj_span/gui.clj
+++ src/clj_span/gui.clj
@@ -104,16 +104,15 @@
                              :numbers  [nb/rv-mean nb/rv-stdev]
                              :varprop  [vp/rv-mean vp/rv-stdev]
                              :randvars [rv/rv-mean rv/rv-stdev])
-        y-dim     (get-rows layer)
-        x-dim     (get-cols layer)
-        mean-img  (render layer scale x-dim y-dim rv-mean)
-        stdev-img (render layer scale x-dim y-dim rv-stdev)]
+        y-dim (get-rows layer)
+        x-dim (get-cols layer)]
     (let [outfile (io/file dirname (str file-prefix "-mean.png"))]
-      (try (ImageIO/write mean-img "png" outfile)
+      (try (ImageIO/write (render layer scale x-dim y-dim rv-mean) "png" outfile)
            (catch IOException e (println "Failed to write mean layer for" file-prefix "to file" (.getName outfile)))))
-    (let [outfile (io/file dirname (str file-prefix "-stdev.png"))]
-      (try (ImageIO/write stdev-img "png" outfile)
-           (catch IOException e (println "Failed to write stdev layer for" file-prefix "to file" (.getName outfile)))))))
+    (if-not (= value-type :numbers)
+      (let [outfile (io/file dirname (str file-prefix "-stdev.png"))]
+        (try (ImageIO/write (render layer scale x-dim y-dim rv-stdev) "png" outfile)
+             (catch IOException e (println "Failed to write stdev layer for" file-prefix "to file" (.getName outfile))))))))
 
 (defn draw-layer [title layer scale value-type]
   (let [[rv-mean rv-stdev] (case value-type
@@ -136,8 +135,9 @@
                                                         *legend-color-height*
                                                         *legend-text-height*
                                                         (* 3 *legend-padding*)))))]
-    (doto (JFrame. (str title " Mean"))               (.add mean-panel)  .pack .show)
-    (doto (JFrame. (str title " Standard Deviation")) (.add stdev-panel) .pack .show)
+    (doto (JFrame. (str title " Mean")) (.add mean-panel) .pack .show)
+    (if-not (= value-type :numbers)
+      (doto (JFrame. (str title " Standard Deviation")) (.add stdev-panel) .pack .show))
     [mean-panel stdev-panel]))
 
 (defn draw-ref-layer [title ref-layer scale value-type]
@@ -163,8 +163,9 @@
                                                         *legend-color-height*
                                                         *legend-text-height*
                                                         (* 3 *legend-padding*)))))]
-    (doto (JFrame. (str title " Mean"))               (.add mean-panel)  .pack .show)
-    (doto (JFrame. (str title " Standard Deviation")) (.add stdev-panel) .pack .show)
+    (doto (JFrame. (str title " Mean")) (.add mean-panel) .pack .show)
+    (if-not (= value-type :numbers)
+      (doto (JFrame. (str title " Standard Deviation")) (.add stdev-panel) .pack .show))
     [mean-panel stdev-panel]))
 
 (defn draw-points [ids scale value-type]
